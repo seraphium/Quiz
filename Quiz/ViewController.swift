6eedf2e260f8160c7b8fa09eee16d4f00ed4bb8c
Jackie Zhang <viperking@163.com>
2016-02-28 15:20:51 +0800

    start animation

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -10,16 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var questionLabel : UILabel!
+    @IBOutlet var answerLabel : UILabel!
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.questionLabel.alpha = 0
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        questionLabel.text = questions[currentQuestionIndex]
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func showNextQuestion(sender: AnyObject)
+    {
+        ++currentQuestionIndex;
+        if currentQuestionIndex == questions.count
+        {
+            currentQuestionIndex = 0;
+        }
+        let question : String = questions[currentQuestionIndex];
+        questionLabel.text = question;
+        answerLabel.text = "???";
+        animateLabelTransitions()
+        
     }
-
-
+    @IBAction func showAnswer(sender: AnyObject)
+    {
+        let answer : String = answers[currentQuestionIndex];
+        answerLabel.text = answer;
+    }
+    let questions : [String] = ["question1", "question2", "question3"]
+    let answers : [String] = ["answer1", "answer2", "answer3"]
+    var currentQuestionIndex : Int = 0;
+    
+    func animateLabelTransitions() {
+        UIView.animateWithDuration(0.5, animations: {
+            self.questionLabel.alpha = 1
+            })
+        
+        
+    }
 }
 
